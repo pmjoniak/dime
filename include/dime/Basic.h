0f8d435fc7f63e08d0aa887cb41f05c3011ea349
barczynsky <barczynsky@users.noreply.github.com>
2014-09-11 15:51:46 +0200

    windows hotfix fixed on linux now, added static lib also

--- include/dime/Basic.h
+++ include/dime/Basic.h
@@ -1,22 +1,22 @@
 /**************************************************************************\
  * Copyright (c) Kongsberg Oil & Gas Technologies AS
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  * Redistributions of source code must retain the above copyright notice,
  * this list of conditions and the following disclaimer.
- * 
+ *
  * Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * Neither the name of the copyright holder nor the names of its
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -30,7 +30,13 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 \**************************************************************************/
 
+#ifdef _WIN32
 #define DIME_NOT_DLL
+#else
+#define _fdopen fdopen
+#define _lseek lseek
+#define _strdup strdup
+#endif
 
 #ifndef DIME_BASIC_H
 #define DIME_BASIC_H
@@ -91,7 +97,7 @@ typedef unsigned int uint32;
 #ifdef macintosh
  char* strdup( const char* );
 #endif
- 
+
 #define ARRAY_NEW(memh, type, num) \
 memh ? (type*) memh->allocMem((num)*sizeof(type)) : new type[num]
 
