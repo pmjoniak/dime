135dff9b29356a7cf452e9a5062944a88491bc05
kyp <kyp108@gmail.com>
2015-06-26 17:05:13 +0200

    update to VS2015

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,4 @@
-call "%programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call "%programfiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 cd dime_vs
 msbuild /p:Configuration=Release_profiling;Platform=Win32 dime_vs.sln
 msbuild /p:Configuration=Release;Platform=Win32 dime_vs.sln
